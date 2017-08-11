a36ea33cd992d3d8c4d4e16bfbfbbe499380968b
xiaoziqingren <1045977704@qq.com>
2017-08-11 21:09:53 +0800

    tcp server #2
    添加tcp server

---
+++ tcp/server/server.go
@@ -0,0 +1,89 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Co.,Ltd..
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/08/11        Sun Anxiang
+ */
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+)
+
+func main() {
+	var tcpAddr *net.TCPAddr
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:9999")
+	if err != nil {
+		fmt.Println("Resolve tcp addr error:", err)
+		panic(err)
+	}
+
+	tcpListener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		fmt.Println("Listen error:", err)
+		panic(err)
+	}
+	defer tcpListener.Close()
+
+	for {
+		tcpConn, err := tcpListener.AcceptTCP()
+		if err != nil {
+			fmt.Println("Accept tcp error:", err)
+			continue
+		}
+
+		fmt.Println("A client connected:" + tcpConn.RemoteAddr().String())
+		go tcpReadPipe(tcpConn)
+	}
+}
+
+func tcpReadPipe(conn *net.TCPConn) {
+	ipStr := conn.RemoteAddr().String()
+	defer func() {
+		fmt.Println("disconnected:" + ipStr)
+		conn.Close()
+	}()
+
+	reader := bufio.NewReader(conn)
+	for {
+		_, err := reader.ReadString('\n')
+		if err != nil {
+			continue
+		}
+		fmt.Println("things", ipStr)
+		tcpWritePipe(conn)
+	}
+}
+
+func tcpWritePipe(conn *net.TCPConn) {
+	msg := time.Now().String() + "\n"
+	b := []byte(msg)
+	conn.Write(b)
+}
