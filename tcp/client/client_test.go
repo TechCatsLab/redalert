8e39b33ec36342e65a8aeb4bcdd0602e5f321d4d
Jsharkc <1773867204@qq.com>
2017-08-11 21:12:31 +0800

    添加 tcp 测试 #2

---
+++ tcp/client/client_test.go
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Co.,Ltd..
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/08/11        Liu jiachang
+ */
+
+package client
+
+import (
+	"sync"
+	"testing"
+)
+
+func BenchmarkTcpClient(b *testing.B) {
+	group := &sync.WaitGroup{}
+	b.StopTimer()
+	for i := 0; i < 101; i++ {
+		TcpClient()
+	}
+
+	group.Add(100)
+	b.StartTimer()
+	for j := 0; j < 100; j++ {
+		go func() {
+			for i := 0; i < b.N; i++ {
+				WriteMessage(Connection[j])
+			}
+
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+}
