bb8522460030baceefb7705f0b8aea30c14f8142
Jsharkc <1773867204@qq.com>
2017-08-14 21:23:36 +0800

    添加每个字段的开始地址 #3

--- udp/protocal/proto.go
+++ udp/protocal/proto.go
@@ -2,13 +2,30 @@ package protocal
 
 const (
 	// Header Size
-	HeaderTypeSize = 1
-	HeaderSize     = 1
-	ReplyStatus    = 1
-	FileSize       = 8
-	PackSize       = 2
-	PackCountSize  = 4
-	PackOrderSize  = 4
-	FileNameSize   = int32(1 << 8)
-	RawHeaderSize  = HeaderTypeSize + HeaderSize + FileSize + PackSize + PackCountSize + PackOrderSize + FileNameSize
+	HeaderTypeSize  = 1 // 包类型
+	HeaderSize      = 2
+	ReplyStatusSize = 1
+	FileSize        = 8
+	PackSize        = 2
+	PackCountSize   = 4
+	PackOrderSize   = 4
+	FixedHeaderSize = HeaderTypeSize + HeaderSize + ReplyStatusSize + FileSize + PackSize + PackCountSize + PackOrderSize
+
+	MinRawHeaderSize    = int32(1<<6) - 1
+	MiddleRawHeaderSize = int32(1<<7) - 1
+	MaxRawHeaderSize    = int32(1<<8) - 1
+
+	MinFileNameSize    = MinRawHeaderSize - FixedHeaderSize
+	MiddleFileNameSize = MiddleRawHeaderSize - FixedHeaderSize
+	MaxFileNameSize    = MaxRawHeaderSize - FixedHeaderSize
+
+	// Offset
+	HeaderTypeOffset  = 0
+	HeaderSizeOffset  = HeaderTypeOffset + HeaderTypeSize
+	ReplyStatusOffset = HeaderSizeOffset + HeaderSize
+	FileSizeOffset    = ReplyStatusOffset + ReplyStatusSize
+	PackSizeOffset    = FileSizeOffset + FileSize
+	PackCountOffset   = PackSizeOffset + PackSize
+	PackOrderOffset   = PackCountOffset + PackCountSize
+	FileNameOffset    = PackOrderOffset + PackOrderSize
 )
